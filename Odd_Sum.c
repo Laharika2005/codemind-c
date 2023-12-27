64299b2871e74a509bc97efacc6e0f4cd17bc9dc
Laharika2005 <147474509+Laharika2005@users.noreply.github.com>
2023-12-26 20:56:30 -0800

    #include<stdio.h> int main() {     int s,i;     scanf("%d",&s);     int arr[s];     for(i=0;i<s;i++)     {         scanf("%d",&arr[i]);     }     int sum=0;     for(i=0;i<s;i++)     {         if(arr[i]%2!=0)         {             sum=sum+arr[i];         }     }     printf("%d",sum); }

---
+++ Odd_Sum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int s,i;
+    scanf("%d",&s);
+    int arr[s];
+    for(i=0;i<s;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int sum=0;
+    for(i=0;i<s;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            sum=sum+arr[i];
+        }
+    }
+    printf("%d",sum);
+}
+    
+    
